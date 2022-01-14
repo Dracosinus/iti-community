5d2d226398b1b3a0ad66913a0071417f658e70ad
Benoit SCHULER <bschuler@avencall.com>
2022-01-14 17:40:40 +0100

    1.5 Verify if username is available

--- src/modules/user/components/user-registration/user-registration.component.ts
+++ src/modules/user/components/user-registration/user-registration.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
 import { UserService } from '../../services/user.service';
+import { UserQueries } from '../../services/user.queries'
 
 class UserRegistrationFormModel {
   username = "";
@@ -22,7 +23,8 @@ export class UserRegistrationComponent implements OnInit {
 
   constructor(
     private router: Router,
-    private userService: UserService
+    private userService: UserService,
+    private userQueries: UserQueries
   ) { }
 
   ngOnInit(): void {
@@ -33,6 +35,11 @@ export class UserRegistrationComponent implements OnInit {
     // TODO  Vérifier que la confirmation de mot de passe correspond au mot de passe
     if (this.form.form.invalid || this.model.password !== this.model.confirmPassword) {
       console.error("Une erreur est survenue. Veuillez vérifier que votre mot de passe et sa confirmation sont identiques.");
+      return;
+    }
+
+    if(await this.userQueries.exists(this.model.username)){
+      console.error("Une erreur est survenue. Ce nom d'utilisateur existe déjà.");
       return;
     }
 
